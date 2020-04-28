92b3a55479996f913286a8daf658d402fb24f8bb
Hugo Wood <hugo.wood@zenika.com>
2020-04-28 23:10:08 +0200

    ♻ change binary file extension

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 rm -r target
 mkdir -p target
-nasm -f bin -o target/boot.flp src/boot.asm
+nasm -f bin -o target/boot.bin src/boot.asm
 mkdir -p target/iso
-cp target/boot.flp target/iso/boot.flp
-genisoimage -no-emul-boot -boot-load-size 4 -o target/boot.iso -b boot.flp target/iso/
+cp target/boot.bin target/iso/boot.bin
+genisoimage -no-emul-boot -boot-load-size 4 -o target/boot.iso -b boot.bin target/iso/
