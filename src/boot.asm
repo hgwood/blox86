f47884db4457c9d145656e9cd4664152a8a7092c
Hugo Wood <hugo.wood@zenika.com>
2020-04-28 23:08:24 +0200

    ♻ remove dead code

--- src/boot.asm
+++ src/boot.asm
@@ -9,10 +9,6 @@ start:
   mov ax, 07C0h		; Set data segment to where we're loaded
   mov ds, ax
 
-
-  mov si, text_string	; Put string position into SI
-  call print_string	; Call our string-printing routine
-
 .wait_for_key_press:
   mov ah, 0
   int 16h
@@ -63,39 +59,6 @@ start:
   mov ah, 02h
   int 10h
   jmp .wait_for_key_press
-
-.print_keys:
-  mov ah, 0
-  int 16h
-  mov bh, ah
-  mov bl, al
-  mov al, bh
-  mov ah, 0eh
-  int 10h
-  mov al, bl
-  mov ah, 0eh
-  int 10h
-  jmp .print_keys
-
-  jmp $			; Jump here - infinite loop!
-
-
-  text_string db 'This is my cool new OS!', 0
-
-
-print_string:			; Routine: output string in SI to screen
-  mov ah, 0eh		; int 10h 'print char' function
-
-.repeat:
-  lodsb			; Get character from string
-  cmp al, 0
-  je .done		; If char is zero, end of string
-  int 10h			; Otherwise, print it
-  jmp .repeat
-
-.done:
-  ret
-
 
 times 510-($-$$) db 0	; Pad remainder of boot sector with 0s
 dw 0xAA55		; The standard PC boot signature
