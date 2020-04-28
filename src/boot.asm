6bf4dfe9b0c2df246fe0c8ba99027dc7995fbe8e
Hugo Wood <hugo.wood@zenika.com>
2020-04-29 01:01:25 +0200

    🔥 remove dead code

--- src/boot.asm
+++ src/boot.asm
@@ -115,56 +115,5 @@ start:
   int 10h
   ret
 
-.wait_for_key_press:
-  mov ah, 0
-  int 16h
-  cmp ah, 48h
-  je .move_cursor_up
-  cmp ah, 50h
-  je .move_cursor_down
-  cmp ah, 4bh
-  je .move_cursor_left
-  cmp ah, 4dh
-  je .move_cursor_right
-  mov ah, 0eh
-  int 10h
-  jmp .wait_for_key_press
-
-.move_cursor_up:
-  mov bh, 0h
-  mov ah, 03h ; get cursor
-  int 10h
-  sub dh, 1
-  mov ah, 02h ; set cursor
-  int 10h
-  jmp .wait_for_key_press
-
-.move_cursor_down:
-  mov bh, 0h
-  mov ah, 03h
-  int 10h
-  add dh, 1
-  mov ah, 02h
-  int 10h
-  jmp .wait_for_key_press
-
-.move_cursor_left:
-  mov bh, 0h
-  mov ah, 03h
-  int 10h
-  sub dl, 1
-  mov ah, 02h
-  int 10h
-  jmp .wait_for_key_press
-
-.move_cursor_right:
-  mov bh, 0h
-  mov ah, 03h
-  int 10h
-  add dl, 1
-  mov ah, 02h
-  int 10h
-  jmp .wait_for_key_press
-
 times 510-($-$$) db 0	; Pad remainder of boot sector with 0s
 dw 0xAA55		; The standard PC boot signature
