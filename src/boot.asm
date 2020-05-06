d805ecf7bc029c13f0f6921f8263abd03561c262
Hugo Wood <hugo.wood@zenika.com>
2020-05-06 23:37:38 +0200

    ♻ move compile-time constants at top of file

--- src/boot.asm
+++ src/boot.asm
@@ -1,3 +1,40 @@
+; constant positions and sizes
+%assign arena_width 64
+%assign arena_height 24
+%assign left_wall_x 0
+%assign top_wall_y 0
+%assign right_wall_x left_wall_x + arena_width + 1
+%assign arena_left left_wall_x + 1
+%assign player_y top_wall_y + arena_height
+%assign arena_bottom player_y + 1
+
+; character constants
+%assign wall_char 58h ; 'X'
+%assign player_char 54h ; 'T'
+%assign ball_char 4fh ; 'O
+%assign empty_char 20h ; ' '
+
+; gameplay constants
+%assign player_speed_multiplier 2
+
+; game state offsets
+%assign player_left_x_offset 0
+%assign player_size_offset 1
+%assign ball_x_offset 2
+%assign ball_y_offset 3
+%assign ball_x_carry_offset 4
+%assign ball_y_carry_offset 5
+%assign ball_speed_x_offset 6
+%assign ball_speed_y_offset 7
+%assign game_over_flag_offset 8
+%assign system_time_offset 10
+%assign system_time_lsw_offset system_time_offset
+%assign system_time_msw_offset system_time_offset + 2
+%assign level_bit_map_offset 32
+
+%assign arena_width_bytes arena_width / 8
+%assign level_size_bytes arena_width_bytes * 2
+
 BITS 16
 
 start:
@@ -15,38 +52,6 @@ start:
   mov si, ds ; set source index pointer
   mov di, ds ; set destination index pointer
 
-  ; constant positions
-  %assign arena_width 64
-  %assign arena_height 24
-  %assign left_wall_x 0
-  %assign top_wall_y 0
-  %assign right_wall_x left_wall_x + arena_width + 1
-  %assign arena_left left_wall_x + 1
-  %assign player_y top_wall_y + arena_height
-  %assign arena_bottom player_y + 1
-
-  ; character constants
-  %assign wall_char 58h ; 'X'
-  %assign player_char 54h ; 'T'
-  %assign ball_char 4fh ; 'O
-  %assign empty_char 20h ; ' '
-
-  ; gameplay constants
-  %assign player_speed_multiplier 2
-
-  ; game state offsets
-  %assign player_left_x_offset 0
-  %assign player_size_offset 1
-  %assign ball_x_offset 2
-  %assign ball_y_offset 3
-  %assign ball_x_carry_offset 4
-  %assign ball_y_carry_offset 5
-  %assign ball_speed_x_offset 6
-  %assign ball_speed_y_offset 7
-  %assign game_over_flag_offset 8
-  %assign system_time_offset 10
-  %assign system_time_lsw_offset system_time_offset
-  %assign system_time_msw_offset system_time_offset + 2
 
   ; initialize game state
   mov [di + player_left_x_offset], byte 10 ; absolute coordinate
