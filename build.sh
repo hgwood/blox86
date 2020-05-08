cd0715d0c633a43db5c552147f3250eb32a36701
Hugo Wood <hugo.wood@zenika.com>
2020-05-08 17:57:04 +0200

    🛠 remove target/iso after iso build

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ nasm -f bin -o target/blox86.bin src/main.asm
 mkdir -p target/iso
 cp target/blox86.bin target/iso/blox86.bin
 genisoimage -no-emul-boot -boot-load-size 4 -o target/blox86.iso -b blox86.bin target/iso/
+rm -r target/iso
