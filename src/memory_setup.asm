92133c43ff0beb51bba0139fb996dba54d431c1e
Hugo Wood <hugo.wood@zenika.com>
2020-05-11 12:42:21 +0200

    🐛 increase memory available for program
    The binary is currently larger than 1 KB so I should allocate for memory for it. It wasn't a problem in practice since the stack is never big enough to reach into program memory and corrupt it.

--- src/memory_setup.asm
+++ src/memory_setup.asm
@@ -1,13 +1,16 @@
-; set up 4K stack space after this bootloader
-mov ax, 7c0h ; 7c0 is the address of the bootloader
-add ax, 1024 ; skip to the end of the bootloader
+%assign bootloader_memory_address 7c0h
+%assign bootloader_size 2048
+%assign stack_size 1024
+%assign stack_pointer_initial_offset stack_size - 1
+
+; set up stack space after this bootloader
+mov ax, bootloader_memory_address
+add ax, bootloader_size ; skip to the end of the bootloader
 mov ss, ax ; set stack segment
-mov sp, 4095 ; set stack pointer to empty stack (remember the stack is reversed)
+mov sp, stack_pointer_initial_offset ; set stack pointer to empty stack (remember the stack is reversed)
 
 ; set up the data segment after the stack
-mov ax, 7c0h ; 7c0 is the address of the bootloader
-add ax, 1024 ; skip to the end of the bootloader
-add ax, 4096 ; skip to the end of the stack
+add ax, stack_size ; skip to the end of the stack
 mov ds, ax ; set data segment
 mov si, ds ; set source index pointer
 mov di, ds ; set destination index pointer
